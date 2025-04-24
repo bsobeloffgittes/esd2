eb69cc41623afbf017cd8d5c6bdb2719c7bde301
bsobeloffgittes <45543964+bsobeloffgittes@users.noreply.github.com>
2025-04-24 17:58:12 -0500

    Servo working correctly

--- ESD2-firmware/src/main.cpp
+++ ESD2-firmware/src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 #include <ESP32_Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 #define SERVO_PIN 18
+
+int pos = 0;    // variable to store the servo position
 
 Servo steering_servo;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  steering_servo.attach(SERVO_PIN); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    steering_servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    steering_servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
